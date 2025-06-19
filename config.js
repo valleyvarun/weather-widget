fcde988ef155f3f16877545e7731792fcae656f9
Varun A. Sridhar <126321540+valleyvarun@users.noreply.github.com>
2025-06-19 14:13:18 +0530

    new fodler made

---
+++ config.js
@@ -0,0 +1,6 @@
+const config = {
+    WEATHER_API_KEY: "4e6e061b1b9edd81067a0b4b995fa6e2"
+}
+
+
+// https://api.openweathermap.org/data/2.5/weather?zip=560062,IN&appid=4e6e061b1b9edd81067a0b4b995fa6e2
